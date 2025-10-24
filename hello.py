ac63ea1ba8b593958de862eb905f106c5835225d
Pavel Prihodko <pa4aprihodko@gmail.com>
2025-10-25 01:24:22 +0300

    /

---
+++ hello.py
@@ -0,0 +1,14 @@
+job_years = 1
+salary = 90000
+message = 'Your premium ratio {}, and your new salary {}'
+
+
+if job_years >=3:
+    coef = 0.30
+elif job_years >=1 and job_years <=3:
+    coef = 0.20
+else:
+    coef = 0.10
+
+salary = salary + salary * coef
+print(message.format(coef, salary))
